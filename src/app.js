7e5d61145e9e3386789a3d2541607480affa7991
titus <titushrangthawng@gmail.com>
2023-08-14 17:38:05 -0400

    createdpublicandsrcfolders

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const hbs = require("hbs");
 
 const app = express();
 app.use(cors());
 
-const PORT = 3000;
-
-app.get("/", (req, res) => {
+const PORT = 4000;
+app.get("", (req, res) => {
     res.send('Successful response.');
 })
 
-app.listen(3000, () => {
+app.get("/about", (req, res) => {
+    res.send('Successful response.');
+})
+
+app.listen(PORT, () => {
     console.log("Listening on port: " + PORT);
 })
