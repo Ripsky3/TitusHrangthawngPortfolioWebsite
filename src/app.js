5f36050d5db2d5e958103a1fa26442dec1492375
titus <titushrangthawng@gmail.com>
2023-08-15 14:20:04 -0400

    goingToHostWebAppOnRender

--- src/app.js
+++ src/app.js
@@ -1,17 +1,53 @@
 const express = require("express");
 const cors = require("cors");
 const hbs = require("hbs");
+const path = require("path");
 
 const app = express();
 app.use(cors());
 
-const PORT = 4000;
+// Define paths for express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// Set up handlebars engine and view location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+
+const PORT = 3000;
+
 app.get("", (req, res) => {
-    res.send('Successful response.');
+    res.render("index", {
+        title: "Titus Hrangthawng"
+    });
 })
 
 app.get("/about", (req, res) => {
-    res.send('Successful response.');
+    res.render("about", {
+        title: "About"
+    });
+})
+
+app.get("/contact", (req, res) => {
+    res.render("contact", {
+        title: "Contact"
+    });
+})
+
+app.get("/help/*", (req, res) => {
+    res.send("help article not found");
+})
+
+app.get("*", (req, res) => {
+    res.render("notfound", {
+        title: "Page not found"
+    });
 })
 
 app.listen(PORT, () => {
