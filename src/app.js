bd1940e5806a4b229f56932dc08db5fccf0d5b8c
titus <titushrangthawng@gmail.com>
2023-11-15 00:24:11 -0500

    Changed PORT to port

--- src/app.js
+++ src/app.js
@@ -16,12 +16,12 @@ app.set("view engine", "hbs");
 app.use(express.static(publicDirectoryPath));
 
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.get("", (req, res) => {
     res.render("index");
 })
 
-app.listen(PORT, () => {
-    console.log("Listening on port: " + PORT);
+app.listen(port, () => {
+    console.log("Listening on port: " + port);
 })
