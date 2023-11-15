53dcb76099a7b1b9edb0032ab5fbb5b84609addd
titus <titushrangthawng@gmail.com>
2023-11-15 00:20:45 -0500

    Got rid of title property in app .get

--- src/app.js
+++ src/app.js
@@ -19,9 +19,7 @@ app.use(express.static(publicDirectoryPath));
 const PORT = process.env.PORT || 3000;
 
 app.get("", (req, res) => {
-    res.render("index", {
-        title: "Titus Hrangthawng"
-    });
+    res.render("index");
 })
 
 app.listen(PORT, () => {
