4c296e4b39c3601f01fc1d717691145d79d45186
titus <titushrangthawng@gmail.com>
2023-11-15 00:06:45 -0500

    Pushing to Heroku

--- src/app.js
+++ src/app.js
@@ -9,12 +9,10 @@ app.use(cors());
 // Define paths for express config
 const publicDirectoryPath = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates/views");
-const partialsPath = path.join(__dirname, "../templates/partials");
 
 // Set up handlebars engine and view location
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
-hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
